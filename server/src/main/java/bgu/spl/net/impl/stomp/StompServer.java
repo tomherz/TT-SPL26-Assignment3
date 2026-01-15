768c130534ed1b535d70dc495d9eed97d0780124
Tamirsi7 <sttamir7@gmail.com>
2026-01-15 12:00:02 +0000

    Maybe finished Server side

--- server/src/main/java/bgu/spl/net/impl/stomp/StompServer.java
+++ server/src/main/java/bgu/spl/net/impl/stomp/StompServer.java
@@ -1,7 +1,12 @@
 package bgu.spl.net.impl.stomp;
 
+import bgu.spl.net.srv.BlockingConnectionHandler;
+
+import java.io.IOException;
+
 import bgu.spl.net.srv.BaseServer;
 import bgu.spl.net.srv.Server;
+import bgu.spl.net.srv.Reactor;
 
 public class StompServer {
 
@@ -14,22 +19,29 @@ public class StompServer {
         int port = Integer.parseInt(args[0]);
         String serverType = args[1];
 
-
-
-        if(serverType.equals("tpc")){
-            new BaseServer<String>(port,() -> new StompMessagingProtocolImpl(),() -> new StompEncoderDecoder(),null){
-                protected void execute(BlockingConnectionHandler<String> handler){
+        if (serverType.equals("tpc")) {
+            try (Server<String> server = new BaseServer<String>(port, () -> new StompMessagingProtocolImpl(),
+                    () -> new StompEncoderDecoder(), null) {
+                protected void execute(BlockingConnectionHandler<String> handler) {
                     new Thread(handler).start();
                 }
-            }.serve();
+            }) {
+                server.serve();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
 
-        else if(serverType.equals("reactor")){
-            //choosing default number of threads, because we didnt get it in args
+        else if (serverType.equals("reactor")) {
+            // choosing default number of threads, because we didnt get it in args
             int numThreads = 5;
-            new Reactor<String>(numThreads, port, () -> new StompMessagingProtocolImpl(), () -> new StompEncoderDecoder(), null).serve();
-        }
-        else{
+            try (Server<String> server = new Reactor<String>(numThreads, port, () -> new StompMessagingProtocolImpl(),
+                    () -> new StompEncoderDecoder(), null)) {
+                server.serve();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } else {
             System.out.println("Unknown server type: " + serverType);
             System.exit(1);
         }
