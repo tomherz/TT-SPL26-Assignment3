96445736c293cb3e3bfd10316bd6a8ae55003c75
tomherz <tomherz@post.bgu.ac.il>
2026-01-17 15:08:36 +0000

    last update client side

--- client/include/StompEncoder.h
+++ client/include/StompEncoder.h
@@ -3,44 +3,58 @@
 
 using namespace std;
 
-class StompEncoder {
+class StompEncoder
+{
     // frame building functions
-    public:
-        static string buildConnect (const string& host, short port, const string& login, const string& passcode) {
-            return "CONNECT\n"
-                   "accept-version:1.2\n"
-                   "host:stomp.cs.bgu.ac.il\n"
-                   "login:" + login + "\n"
-                   "passcode:" + passcode + "\n"
-                   "\n";
-        }
-    
-        static string buildSubscribe (const string& topic, int id, int receipt) {
-            return "SUBSCRIBE\n"
-                   "destination:/" + topic + "\n"
-                   "id:" + to_string(id) + "\n"
-                   "receipt:" + to_string(receipt) + "\n"
-                   "\n";
-        }
+public:
+    static string buildConnect(const string &host, short port, const string &login, const string &passcode)
+    {
+        return "CONNECT\n"
+               "accept-version:1.2\n"
+               "host:stomp.cs.bgu.ac.il\n"
+               "login:" +
+               login + "\n"
+                       "passcode:" +
+               passcode + "\n"
+                          "\n";
+    }
 
-        static string buildUnsubscribe (int id, int receipt) {
-            return "UNSUBSCRIBE\n"
-                   "id:" + to_string(id) + "\n"
-                   "receipt:" + to_string(receipt) + "\n"
-                   "\n";
-        }
+    static string buildSubscribe(const string &topic, int id, int receipt)
+    {
+        return "SUBSCRIBE\n"
+               "destination:/" +
+               topic + "\n"
+                       "id:" +
+               to_string(id) + "\n"
+                               "receipt:" +
+               to_string(receipt) + "\n"
+                                    "\n";
+    }
 
-        static string buildSend (const string& topic, const string& msg) {
-            return "SEND\n"
-                   "destination:/" + topic + "\n"
-                   "\n" +
-                   msg + "\n";
-        }
-        
-        static string buildDisconnect(int reciept) {
-            return "DISCONNECT\n"
-                    "receipt:" + to_string(reciept) + "\n"
-                    "\n";
-        }
-}
-;
+    static string buildUnsubscribe(int id, int receipt)
+    {
+        return "UNSUBSCRIBE\n"
+               "id:" +
+               to_string(id) + "\n"
+                               "receipt:" +
+               to_string(receipt) + "\n"
+                                    "\n";
+    }
+
+    static string buildSend(const string &topic, const string &msg)
+    {
+        return "SEND\n"
+               "destination:/" +
+               topic + "\n"
+                       "\n" +
+               msg + "\n";
+    }
+
+    static string buildDisconnect(int reciept)
+    {
+        return "DISCONNECT\n"
+               "receipt:" +
+               to_string(reciept) + "\n"
+                                    "\n";
+    }
+};
